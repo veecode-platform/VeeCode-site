c8acba8d18f06c89282336b94fe40d212d70959d
ValberJunior <valberjr1992@gmail.com>
2025-05-12 18:15:50 -0300

    Update primary color in Tailwind configuration for improved styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,10 +25,11 @@ const config: Config = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
+        primary: "#33FFCE",
+        // primary: {
+        //   DEFAULT: "hsl(var(--primary))",
+        //   foreground: "hsl(var(--primary-foreground))",
+        // },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
