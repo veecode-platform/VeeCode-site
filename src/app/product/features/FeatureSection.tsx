c57cc246f89acad4c3b3d0f8d5254c415821111a
ValberJunior <valberjr1992@gmail.com>
2025-05-12 11:18:23 -0300

    Replace <img> with <Image> component for optimized image handling

--- src/app/product/features/FeatureSection.tsx
+++ src/app/product/features/FeatureSection.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FeatureCard from "./FeatureCard";
+import Image from "next/image";
 
 interface Feature {
   icon: string;
@@ -22,9 +23,11 @@ const FeatureSection: React.FC<FeatureSectionProps> = ({
       <div className="gap-5 flex max-md:flex-col max-md:items-stretch">
         <div className="w-6/12 max-md:w-full max-md:ml-0">
           <div className="flex grow items-stretch gap-7 text-black flex-wrap max-md:max-w-full max-md:mt-10">
-            <img
+            <Image
               src="https://cdn.builder.io/api/v1/image/assets/25b967a33ef84309975ff5f0a5545581/97c27ee2d33b37f19e1b2e4b3ea5ce41f44e9b56?placeholderIfAbsent=true"
               alt="Vertical line"
+              width={5}
+              height={100}
               className="aspect-[0] object-contain w-[5px] shrink-0"
             />
             <div className="grow shrink-0 basis-0 w-fit mt-[5px] max-md:max-w-full">
@@ -46,9 +49,11 @@ const FeatureSection: React.FC<FeatureSectionProps> = ({
         </div>
         <div className="w-6/12 ml-5 max-md:w-full max-md:ml-0">
           <div className="flex grow items-stretch gap-[40px_48px] text-black flex-wrap max-md:max-w-full max-md:mt-10">
-            <img
+            <Image
               src="https://cdn.builder.io/api/v1/image/assets/25b967a33ef84309975ff5f0a5545581/35e129d14a7813f0e3257809f5669c57edd6787c?placeholderIfAbsent=true"
               alt="Vertical line"
+              width={5}
+              height={100}
               className="aspect-[0] object-contain w-[5px] shrink-0"
             />
             <div className="flex flex-col items-stretch grow shrink-0 basis-0 w-fit my-auto max-md:max-w-full">
