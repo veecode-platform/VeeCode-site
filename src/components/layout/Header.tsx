17ce7d841ffc7683208973f5cf52b3c0939443be
ValberJunior <valberjr1992@gmail.com>
2025-05-19 13:22:05 -0300

    feat: Update Header component to support multilingual translations

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,8 +4,12 @@ import Link from "next/link";
 import VeeCodeLogo from "../VeeCodeLogo";
 import MenuMobile from "./ui/Header-mobile";
 import SwitchLanguage from "../ui/SwitchLanguage";
+import { getTranslations } from "next-intl/server";
+import { PatternComponentProps } from "@/lib/@types/patternComponentProps";
 
-const Header: React.FC = () => {
+const Header: React.FC<PatternComponentProps> = async ({ locale }) => {
+  const t = await getTranslations({ locale, namespace: "header" });
+
   return (
     <header className="flex justify-center items-center self-stretch h-[94px] w-full gap-[40px_100px] flex-wrap bg-neutral-700 px-10 py-5 max-md:max-w-full max-md:px-5 z-[9999999] fixed top-0">
       <div className="w-full flex items-center justify-between  2xl:w-[80%]  2xl:mx-auto">
@@ -14,31 +18,31 @@ const Header: React.FC = () => {
         </Link>
         <nav className="hidden 2xl:flex self-stretch min-w-60 items-center gap-[40px_70px] text-xl text-neutral-50 font-normal flex-wrap my-auto max-md:max-w-full">
           <Link
-            href="product"
+            href="/product"
             className="text-neutral-50 self-stretch my-auto hover:text-[rgba(51,255,205,1)]"
           >
-            Produto
+            {t("product")}
           </Link>
           <Link
-            href="solutions"
+            href="/solutions"
             className="text-neutral-50 self-stretch my-auto hover:text-[rgba(51,255,205,1)]"
           >
-            Soluções
+            {t("solutions")}
           </Link>
           <Link
-            href="resources"
+            href="/resources"
             className="text-neutral-50 self-stretch gap-2.5 whitespace-nowrap my-auto p-2.5 hover:text-[rgba(51,255,205,1)]"
           >
-            Recursos
+            {t("resources")}
           </Link>
           <Link
-            href="comparison"
+            href="/comparison"
             className="text-neutral-50 self-stretch my-auto hover:text-[rgba(51,255,205,1)]"
           >
-            Veecode vs. Backstage
+            {t("comparison")}
           </Link>
           <Button variant="secondary" size="default">
-            Acesse a Demo
+            {t("demo")}
           </Button>
         </nav>
         <SwitchLanguage />
