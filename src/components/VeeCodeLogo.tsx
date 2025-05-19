b3a3eafe4f0e9c8b58c1cd01ddf70adce28f4edb
ValberJunior <valberjr1992@gmail.com>
2025-05-19 10:17:43 -0300

    fix(VeeCodeLogo): set default width and height props, improve className handling

--- src/components/VeeCodeLogo.tsx
+++ src/components/VeeCodeLogo.tsx
@@ -7,14 +7,16 @@ interface VeeCodeLogoProps {
   height?: number;
 }
 
-const VeeCodeLogo: React.FC<VeeCodeLogoProps> = ({ width, height }) => (
+const VeeCodeLogo: React.FC<VeeCodeLogoProps> = ({
+  width = 170,
+  height = 53.03,
+}) => (
   <Image
     src={VEECODE_LOGO}
     alt="VeeCode Logo"
-    className="aspect-[3.21] object-contain w-[170px] self-stretch shrink-0 my-auto"
-    width={width ?? 170}
-    height={height ?? 53.03}
-    unoptimized
+    className={`aspect-[3.21] object-contain w-[${width}px] self-stretch shrink-0`}
+    width={width}
+    height={height}
   />
 );
 
