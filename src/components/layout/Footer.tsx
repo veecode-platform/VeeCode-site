752364e9c0875ea72aa7623b21fc7b8aaa896a5d
ValberJunior <valberjr1992@gmail.com>
2025-05-19 10:16:04 -0300

    feat: Add SwitchLanguage component for language selection functionality

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,151 +7,107 @@ const Footer: React.FC = () => {
     <footer className="items-center self-stretch flex w-full flex-col bg-black max-md:max-w-full max-md:px-5 pt-20 pb-10 px-8">
       <div className="w-full max-w-[1204px] max-md:max-w-full">
         <div className="w-full gap-[60px] pb-28 max-md:max-w-full max-md:pb-[100px]">
-          <div className="content-start align-top justify-start flex-wrap flex w-full gap-8 max-md:max-w-full">
-            <div className="min-w-60">
-              <VeeCodeLogo width={197} height={75} />
+          <div className="flex flex-col content-center justify-center align-middle md:flex-row text-center md:content-start md:align-top md:justify-start flex-wrap w-full gap-8 max-md:max-w-full">
+            <div className="md:min-w-60 flex justify-center items-center md:items-start md:justify-start md:align-top md:mt-[-100px]">
+              <VeeCodeLogo width={207} height={85} />
             </div>
-            <div className="min-w-40 justify-center flex flex-col text-base text-white flex-1 shrink basis-[0%] gap-4">
-              <h3 className="self-stretch gap-2 font-bold whitespace-nowrap leading-[1.2]">
+            <div className="min-w-40 flex justify-center flex-col text-base text-white flex-1 shrink basis-[0%] gap-4">
+              <h3 className="self-stretch md:text-left gap-2 font-bold whitespace-nowrap leading-[1.2]">
                 Sobre
               </h3>
-              <nav className="justify-center flex flex-col font-medium text-center leading-none gap-2 mt-4">
+              <nav className="flex justify-center flex-col font-medium text-center leading-none gap-2 mt-4">
                 <Link
                   href="#how-it-works"
-                  className="flex hover:text-[rgba(51,255,205,1)]"
+                  className="text-neutral-300 hover:text-[rgba(51,255,205,1)] text-center md:text-left"
                 >
-                  <div className="items-center flex gap-1">
-                    <span className="self-stretch overflow-hidden my-auto">
-                      Como funciona
-                    </span>
-                  </div>
+                  Como funciona
                 </Link>
                 <Link
                   href="#support"
-                  className="flex whitespace-nowrap mt-2 hover:text-[rgba(51,255,205,1)]"
+                  className="text-neutral-300 hover:text-[rgba(51,255,205,1)] text-center md:text-left"
                 >
-                  <div className="items-center flex gap-1">
-                    <span className="self-stretch overflow-hidden my-auto">
-                      Suporte
-                    </span>
-                  </div>
+                  Suporte
                 </Link>
                 <Link
                   href="#community"
-                  className="self-stretch flex mt-2 hover:text-[rgba(51,255,205,1)]"
+                  className="text-neutral-300 hover:text-[rgba(51,255,205,1)] text-center md:text-left"
                 >
-                  <div className="items-center flex gap-1">
-                    <span className="self-stretch overflow-hidden my-auto">
-                      Junte-se à comunidade
-                    </span>
-                  </div>
+                  Junte-se à comunidade
                 </Link>
                 <Link
                   href="#contact"
-                  className="flex mt-2 hover:text-[rgba(51,255,205,1)]"
+                  className="text-neutral-300 hover:text-[rgba(51,255,205,1)] text-center md:text-left"
                 >
-                  <div className="items-center flex gap-1">
-                    <span className="self-stretch overflow-hidden my-auto">
-                      Fale Conosco
-                    </span>
-                  </div>
+                  Fale Conosco
                 </Link>
               </nav>
             </div>
             <div className="min-w-40 justify-center flex flex-col flex-1 shrink basis-[0%] gap-4">
-              <h3 className="self-stretch gap-2 text-base text-white font-bold whitespace-nowrap leading-[1.2]">
+              <h3 className="self-stretch  md:text-left gap-2 text-base text-white font-bold whitespace-nowrap leading-[1.2]">
                 Recursos
               </h3>
               <nav className="justify-center flex flex-col gap-2 mt-4">
                 <Link
                   href="#documentation"
-                  className="flex text-base text-white font-medium whitespace-nowrap text-center leading-none hover:text-[rgba(51,255,205,1)]"
+                  className="text-neutral-300 hover:text-[rgba(51,255,205,1)] text-center md:text-left"
                 >
-                  <div className="items-center flex gap-1">
-                    <span className="self-stretch overflow-hidden my-auto">
-                      Documentação
-                    </span>
-                  </div>
+                  Documentação
                 </Link>
                 <Link
                   href="#vkdr"
-                  className="flex text-base text-white font-medium whitespace-nowrap text-center leading-none mt-2 hover:text-[rgba(51,255,205,1)]"
+                  className="text-neutral-300 hover:text-[rgba(51,255,205,1)] text-center md:text-left"
                 >
-                  <div className="items-center flex gap-1">
-                    <span className="self-stretch overflow-hidden my-auto">
-                      VKDR
-                    </span>
-                  </div>
+                  VKDR
                 </Link>
                 <Link
                   href="#devportal"
-                  className="flex text-base text-white font-medium whitespace-nowrap text-center leading-none mt-2 hover:text-[rgba(51,255,205,1)]"
+                  className="text-neutral-300 hover:text-[rgba(51,255,205,1)] text-center md:text-left"
                 >
-                  <div className="items-center flex gap-1">
-                    <span className="self-stretch overflow-hidden my-auto">
-                      DevPortal
-                    </span>
-                  </div>
+                  DevPortal
                 </Link>
                 <Link
                   href="#plugins"
-                  className="flex text-base text-white font-medium whitespace-nowrap text-center leading-none mt-2 hover:text-[rgba(51,255,205,1)]"
+                  className="text-neutral-300 hover:text-[rgba(51,255,205,1)] text-center md:text-left"
                 >
                   Plugins
                 </Link>
               </nav>
             </div>
             <div className="min-w-40 justify-center flex flex-col text-base text-white flex-1 shrink basis-[0%] gap-4">
-              <h3 className="self-stretch gap-2 font-bold whitespace-nowrap leading-[1.2]">
+              <h3 className="self-stretch md:text-left gap-2 font-bold whitespace-nowrap leading-[1.2]">
                 Mais
               </h3>
               <nav className="justify-center flex flex-col font-medium text-center leading-none gap-2 mt-4">
                 <Link
                   href="#blog"
-                  className="flex whitespace-nowrap hover:text-[rgba(51,255,205,1)]"
+                  className="text-neutral-300 hover:text-[rgba(51,255,205,1)] text-center md:text-left"
                 >
-                  <div className="items-center flex gap-1">
-                    <span className="self-stretch overflow-hidden my-auto">
-                      Blog
-                    </span>
-                  </div>
+                  Blog
                 </Link>
                 <Link
                   href="#privacy"
-                  className="self-stretch flex mt-2 hover:text-[rgba(51,255,205,1)]"
+                  className="text-neutral-300 hover:text-[rgba(51,255,205,1)] text-center md:text-left"
                 >
-                  <div className="items-center flex gap-1">
-                    <span className="self-stretch overflow-hidden my-auto">
-                      Políticas de Privacidade
-                    </span>
-                  </div>
+                  Políticas de Privacidade
                 </Link>
                 <Link
                   href="#terms"
-                  className="flex mt-2 hover:text-[rgba(51,255,205,1)]"
+                  className="text-neutral-300 hover:text-[rgba(51,255,205,1)] text-center md:text-left"
                 >
-                  <div className="items-center flex gap-1">
-                    <span className="self-stretch overflow-hidden my-auto">
-                      Termos de Serviço
-                    </span>
-                  </div>
+                  Termos de Serviço
                 </Link>
                 <Link
                   href="#faq"
-                  className="flex whitespace-nowrap mt-2 hover:text-[rgba(51,255,205,1)]"
+                  className="text-neutral-300 hover:text-[rgba(51,255,205,1)] text-center md:text-left"
                 >
-                  <div className="items-center flex gap-1">
-                    <span className="self-stretch overflow-hidden my-auto">
-                      FAQ
-                    </span>
-                  </div>
+                  FAQ
                 </Link>
               </nav>
             </div>
           </div>
         </div>
         <div className="flex min-h-px w-full flex-col items-stretch justify-center max-md:max-w-full">
-          <div className="border min-h-px w-full bg-[#D2D5E2] border-[rgba(210,213,226,1)] border-solid max-md:max-w-full" />
+          <div className="border min-h-px w-full bg-[#D2D5E2] border-neutral-700 border-solid max-md:max-w-full" />
         </div>
         <div className="self-stretch w-full content-center flex-wrap gap-[24px_var(--inline-2xl,24px)] text-sm text-white font-normal leading-[1.4] py-[20px] max-md:max-w-full text-center">
           <p>Copyright © 2025 Todos os direitos reservados</p>
---
+++ src/components/ui/SwitchLanguage.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import React from "react";
+// import { useTranslations } from "next-intl";
+import { IoLanguageOutline } from "react-icons/io5";
+// import { usePathname, useRouter } from "@/i18n/routing";
+
+const SwitchLanguage = () => {
+  const [dropdownShow, setDropdownShow] = React.useState<boolean>(false);
+  // const t = useTranslations("localeSwitcher");
+
+  // const pathName = usePathname();
+  // const router = useRouter();
+
+  const handleToggleDropdown = () => setDropdownShow(!dropdownShow);
+
+  const handleChooseLanguage = (lgn: string) => {
+    handleToggleDropdown();
+    console.log("lgn", lgn); // TODO remove this log
+    // router.push(pathName as any, { locale: lgn });
+  };
+
+  return (
+    <div className="relative flex items-center justify-center">
+      <button
+        aria-label="menu-language"
+        className="text-2xl text-gray-300 hover:text-[#33ffce] cursor-pointer rounded-md w-[50px] h-[50px] border-[0.5px] border-[#ffffff2e] flex items-center justify-center "
+        onClick={handleToggleDropdown}
+      >
+        <IoLanguageOutline />
+      </button>
+
+      {dropdownShow && (
+        <div className="absolute top-[101%] left-[55%] p-0.3 bg-[rgba(30,30,30,0.90)]  border-[0.5px] border-[#ffffff2e] rounded-sm">
+          <ul className="w-full flex flex-col items-start justify-center gap-0.5">
+            <li
+              className="p-1 text-lg text-gray-300 rounded-sm cursor-pointer w-[100%] h-8 flex items-center justify-center"
+              onClick={() => handleChooseLanguage("pt")}
+            >
+              {/* {t.rich("pt", {
+                p: (chunks) => <p>{chunks}</p>,
+                span: (chunks) => <span>{chunks}</span>,
+              })} */}
+              <p>PT</p> <span>🇧🇷</span>
+            </li>
+            <li
+              className="py-1 px-2 text-lg text-gray-300 rounded-sm cursor-pointer w-[100%] h-8 flex items-center justify-center"
+              onClick={() => handleChooseLanguage("en")}
+            >
+              {/* {t.rich("en", {
+                p: (chunks) => <p>{chunks}</p>,
+                span: (chunks) => <span>{chunks}</span>,
+              })} */}
+              <p>EN</p> <span>🇬🇧</span>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SwitchLanguage;
