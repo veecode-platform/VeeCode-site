c6b2d12c35fce68fd42c6e3e36b8ec4b0119bb64
ValberJunior <valberjr1992@gmail.com>
2025-05-20 15:35:20 -0300

    fix: Correct postcss.config.mjs syntax for Tailwind CSS plugin configuration

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
-/* eslint-disable import/no-anonymous-default-export */
-/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   plugins: {
-    "@tailwindcss/postcss": {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
