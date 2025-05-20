228e6d09aca360a7209e8308c733630064a92513
ValberJunior <valberjr1992@gmail.com>
2025-05-20 06:25:20 -0300

    feat: Update product page and feature section with English translations and improve feature mapping

--- src/app/[locale]/product/components/FeatureSection.tsx
+++ src/app/[locale]/product/components/FeatureSection.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import FeatureCard from "./FeatureCard";
 interface Feature {
+  id: number;
   icon: string;
   title: string;
   description: string;
@@ -20,8 +21,8 @@ const FeatureSection: React.FC<FeatureSectionProps> = ({
     <section className="self-center w-full max-w-[1496px] ml-2.5 my-[213px] max-md:max-w-full max-md:my-10">
       <div className="gap-5 flex max-md:flex-col max-md:items-stretch">
         <div className="w-[50%] h-full flex flex-col justify-between gap-[284px] border-l-1 border-[#33FFCE]">
-          {leftFeatures.map((feature, index) => (
-            <div key={index}>
+          {leftFeatures.map((feature) => (
+            <div key={feature.id}>
               <FeatureCard
                 icon={feature.icon}
                 title={feature.title}
@@ -32,8 +33,8 @@ const FeatureSection: React.FC<FeatureSectionProps> = ({
           ))}
         </div>
         <div className="w-[50%] flex flex-col justify-center gap-[284px] border-l-1 border-[#33FFCE]">
-          {rightFeatures.map((feature, index) => (
-            <div key={index}>
+          {rightFeatures.map((feature) => (
+            <div key={feature.id}>
               <FeatureCard
                 icon={feature.icon}
                 title={feature.title}
--- src/app/[locale]/product/page.tsx
+++ src/app/[locale]/product/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import FeatureSection from "./components/FeatureSection";
+import { getTranslations } from "next-intl/server";
+import { PageProps } from "@/lib/@types/pageProps";
 // import NewsletterForm from "../ui/product/NewsletterForm";
 // import ContactForm from "../ui/product/ContactForm";
 
@@ -9,47 +11,50 @@ const Icon3 = "/assets/product/icon3.svg";
 const Icon4 = "/assets/product/icon4.svg";
 const Icon5 = "/assets/product/icon5.svg";
 
-export default function ProductPage() {
+export default async function ProductPage({ params }: PageProps) {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: "product" });
+
   // Feature data for the left column
   const leftFeatures = [
     {
+      id: 1,
       icon: Icon1,
-      title: "Catálogo de APIs",
-      description:
-        "O Devportal by VeeCode Platform é a plataforma ideal para centralizar e catalogar suas APIs de maneira eficiente. Com o Devportal você pode ver, testar e gerenciar todas as suas APIs em um único lugar enquanto também tem acesso à documentação e plugins necessários para desenvolver o seu projeto, app ou feature.",
-      altText: "API Icon",
+      title: t("card1.title"),
+      description: t("card1.description"),
+      altText: t("card1.title"),
     },
     {
+      id: 2,
       icon: Icon2,
-      title: "Kong Service Manager",
-      description:
-        "Gerencie e administre informações sobre os serviços do Kong. Liste e manipule rotas, gerencie plugins sem sair do DevPortal. Acesse detalhes dos serviços e",
-      altText: "Kong Icon",
+      title: t("card3.title"),
+      description: t("card3.description"),
+      altText: t("card3.title"),
     },
     {
+      id: 3,
       icon: Icon3,
-      title: "VeeCode RBAC",
-      description:
-        "O VeeCode RBAC permite a configuração de acessos que você precisa para garantir acesso customizado. Selecione quem pode ou não, ver itens, executar tarefas ou realizar mudanças, garantindo mostrar apenas informações pertinentes e relevantes para cada tipo de usuário.",
-      altText: "RBAC Icon",
+      title: t("card5.title"),
+      description: t("card5.description"),
+      altText: t("card5.title"),
     },
   ];
 
   // Feature data for the right column
   const rightFeatures = [
     {
+      id: 4,
       icon: Icon4,
-      title: "Software Templates",
-      description:
-        "Com o Devportal você pode customizar seu ambiente com templates para desenvolvimento e infraestrutura, assim como tem a liberdade para personalizar pipelines. A plataforma oferece uma variedade de templates pré-configurados que aceleram o processo de desenvolvimento e garantem consistência no seu produto",
-      altText: "Templates Icon",
+      title: t("card2.title"),
+      description: t("card2.description"),
+      altText: t("card2.title"),
     },
     {
+      id: 5,
       icon: Icon5,
-      title: "VeeCode Docs",
-      description:
-        "Centralize toda a documentação em um só lugar. O VeeCode busca por documentações em todos os repositórios e reúne todas as entrada em seu DevPortal, permitindo um acesso amplo e organizado.",
-      altText: "Docs Icon",
+      title: t("card4.title"),
+      description: t("card4.description"),
+      altText: t("card4.title"),
     },
   ];
 
