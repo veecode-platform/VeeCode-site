c6762655d7d30aa94d275e4e04d8e6c4756baa86
ValberJunior <valberjr1992@gmail.com>
2025-05-20 08:55:56 -0300

    feat: Replace PiCheckFatFill with Image component for CheckIcon to enhance visual representation

--- src/components/CheckIcon.tsx
+++ src/components/CheckIcon.tsx
@@ -1,5 +1,15 @@
-import { PiCheckFatFill } from "react-icons/pi";
+import { CHECK_ICON } from "@/lib/constants";
+import Image from "next/image";
 
-const CheckIcon = () => <PiCheckFatFill size={20} className="text-teal-500" />;
+const CheckIcon = () => (
+  <Image
+    src={CHECK_ICON}
+    alt=""
+    className="aspect-[1] object-contain w-6 shrink-0"
+    width={24}
+    height={24}
+    unoptimized
+  />
+);
 
 export default CheckIcon;
