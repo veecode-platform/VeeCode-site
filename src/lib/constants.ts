9d403f109c3f052e5d187b801b4a8635301ece75
ValberJunior <valberjr1992@gmail.com>
2025-05-20 08:55:27 -0300

    feat: Add CHECK_ICON and NOT_ICON constants for improved asset management

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,4 @@
 export const VEECODE_LOGO = "/assets/pattern/logo.svg";
+export const CHECK_ICON = "/assets/icons/Check.svg";
+export const NOT_ICON = "/assets/icons/not.svg";
 export const LANGUAGES = ["en", "pt"];
