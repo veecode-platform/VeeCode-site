5887ffd00b74e0d56ce2cd30797e1e378876dda6
ValberJunior <valberjr1992@gmail.com>
2025-05-21 12:19:38 -0300

    fix: Refactor loading component layout and update PageNotFound image dimensions for improved styling

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,29 +1,24 @@
-import DefaultPage from "@/components/layout/DefaultPage";
 import Image from "next/image";
 
 const LoadingAnimation = `/assets/icons/loading.gif`;
 
 const loading = () => {
   return (
-    <DefaultPage title="">
-      <div className="w-[100vw] h-[75vh] flex justify-center items-center">
-        <div className="flex justify-center items-center w-full h-full">
-          <Image
-            src={LoadingAnimation}
-            priority
-            alt="Loading animation"
-            width={200}
-            height={62}
-            style={{
-              width: "100%",
-              height: "auto",
-            }}
-            unoptimized
-            className="w-[60%] md:max-w-[280px] xl:max-w-[480px]"
-          />
-        </div>
-      </div>
-    </DefaultPage>
+    <main className="w-[100vw] min-h-[85vh] self-center flex justify-center items-center">
+      <Image
+        src={LoadingAnimation}
+        priority
+        alt="Loading animation"
+        width={200}
+        height={62}
+        style={{
+          width: "100%",
+          height: "auto",
+        }}
+        unoptimized
+        className="w-[60%] md:max-w-[280px] xl:max-w-[480px]"
+      />
+    </main>
   );
 };
 
--- src/components/layout/PageNotFound.tsx
+++ src/components/layout/PageNotFound.tsx
@@ -5,15 +5,15 @@ import { GoHome } from "react-icons/go";
 const ImageNotFound = `/assets/notFound/404.png`;
 
 const PageNotFound = () => (
-  <main className="w-[100vw] h-[100vh] bg-neutral-400 flex justify-center items-center">
+  <main className="w-[100vw] h-[100vh] bg-neutral-300 flex justify-center items-center">
     <section className="w-[90%] min-w-[350px] md:w-[80%] lg:w-[85%] 2xl:w-[1200px]">
       <div className="w-[100%] flex justify-center items-center flex-col gap-8">
         <Image
           src={ImageNotFound}
           priority
           alt="Page Not Found Image"
-          width={200}
-          height={62}
+          width={300}
+          height={200}
           style={{
             width: "100%",
             height: "auto",
