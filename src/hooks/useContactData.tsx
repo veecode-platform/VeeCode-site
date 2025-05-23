ccede9eca0927b57c0195de8436399f645fc5baf
ValberJunior <valberjr1992@gmail.com>
2025-05-23 15:35:20 -0300

    fix: Standardize string quotes in useContactData and api service files

--- src/hooks/useContactData.tsx
+++ src/hooks/useContactData.tsx
@@ -1,25 +1,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import api from '@/services/api';
+import api from "@/services/api";
 
 export const UseContactData = async (values: any) => {
   try {
-    const interests = [''];
-    if (values.vkpr) interests.push('vkpr');
-    if (values.safiracli) interests.push('safira-cli');
-    if (values.support) interests.push('support');
-    if (values.devportal) interests.push('devportal');
+    const interests = [""];
+    if (values.vkpr) interests.push("vkpr");
+    if (values.safiracli) interests.push("safira-cli");
+    if (values.support) interests.push("support");
+    if (values.devportal) interests.push("devportal");
 
     const model = {
-      'full-name': values.name,
-      organization: values.company ?? '',
-      'e-mail': values.email,
+      "full-name": values.name,
+      organization: values.company ?? "",
+      "e-mail": values.email,
       interests: interests,
       question: values.question,
-      'job-title': values.title,
-      'contact-type': values.type ?? 'TRIAL',
+      "job-title": values.title,
+      "contact-type": values.type ?? "TRIAL",
     };
 
-    const { data } = await api.post('/contacts', model);
+    const { data } = await api.post("/contacts", model);
 
     return data;
   } catch (error: unknown) {
