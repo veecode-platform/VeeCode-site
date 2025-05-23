8dc9dad986a3e1fc279e149e469de4a7338a6ac8
ValberJunior <valberjr1992@gmail.com>
2025-05-23 15:49:34 -0300

    fix: Improve layout and spacing in CallToAction component for better presentation

--- src/app/[locale]/home/CallToAction.tsx
+++ src/app/[locale]/home/CallToAction.tsx
@@ -18,18 +18,18 @@ const CallToAction: React.FC<CallToActionProps> = ({
   descriptionExternalLink,
 }) => {
   return (
-    <section className="flex flex-col items-center my-[131px] pb-6 max-md:my-10">
-      <div className="min-h-[188px] w-[1032px] max-w-full mb-30 md:mb-0">
+    <section className="flex flex-col items-center my-[131px] max-md:my-15 md:py-0 gap-10 md:gap-10">
+      <div className="w-[1032px] max-w-[90vw]">
         <h2 className="text-black text-[40px] font-bold max-md:max-w-full text-center">
           {title}
         </h2>
         {subtitle && (
-          <p className="text-[#1e1e1e] text-center text-xl font-normal max-md:max-w-full">
+          <p className="text-[#1e1e1e] text-center text-xl font-normal max-md:max-w-full mt-5 md:mt-1">
             {subtitle}
           </p>
         )}
       </div>
-      <div className="z-10 flex items-center gap-[23px] justify-center mt-[-68px] min-h-11 w-[374px] max-w-full  text-base">
+      <div className="z-10 flex items-center gap-[23px] justify-center min-h-11 w-[374px] max-w-full  text-base">
         <NavigationLink href="/contact-us">
           <Button variant="default" size="sm" aria-label={buttonLabel1}>
             {buttonLabel1}
