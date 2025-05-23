e32262c90433aea41d951fd547e1a313c52affd3
ValberJunior <valberjr1992@gmail.com>
2025-05-23 17:46:52 -0300

    fix: Update customer section label for consistency and add Sankhya logo to ClientLogos component

--- src/app/[locale]/home/ClientLogos.tsx
+++ src/app/[locale]/home/ClientLogos.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 import React from "react";
 
 const VertigoLogo = "/assets/home/vertigo-logo.webp";
-// const AmbimaLogo = "/assets/home/ambima-logo.webp";
+const AmbimaLogo = "/assets/home/ambima-logo.webp";
+const SankhyaLogo = "/assets/home/sankhya.webp";
 const CredSystemLogo = "/assets/home/credsystem-logo.webp";
 
 interface ClientLogosProps {
@@ -11,7 +12,7 @@ interface ClientLogosProps {
 
 const ClientLogos: React.FC<ClientLogosProps> = ({ label }) => {
   return (
-    <section className="flex flex-col items-center">
+    <section className="flex flex-col items-center my-20 py-5 bg-neutral-50 md:bg-transparent md:my-0 md:py-0">
       <p className="text-[#4B5162] text-center text-base font-normal leading-[1.6] opacity-80 mt-[125px] max-md:max-w-full max-md:mt-10">
         {label}
       </p>
@@ -24,14 +25,22 @@ const ClientLogos: React.FC<ClientLogosProps> = ({ label }) => {
           className="aspect-[2.24] object-contain w-[161px] self-stretch shrink-0 my-auto"
           unoptimized
         />
-        {/* <Image
+        <Image
+          src={SankhyaLogo}
+          alt="Client Logo 1"
+          width={183}
+          height={97}
+          className="aspect-[2.24] object-contain w-[161px] self-stretch shrink-0 my-auto"
+          unoptimized
+        />
+        <Image
           src={AmbimaLogo}
           alt="Client Logo 2"
           width={183}
           height={97}
           className="aspect-[1.89] object-contain w-[183px] self-stretch shrink-0 my-auto"
           unoptimized
-        /> */}
+        />
         <Image
           src={CredSystemLogo}
           alt="Client Logo 3"
