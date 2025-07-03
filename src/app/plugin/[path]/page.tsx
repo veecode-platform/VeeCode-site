e43308e83115c00ceb429493a5284fd17e24df57
ValberJunior <valberjr1992@gmail.com>
2025-07-03 13:36:09 -0300

    fix: Enhance PluginPageRedirect with static params generation and type refinement

--- src/app/plugin/[path]/page.tsx
+++ src/app/plugin/[path]/page.tsx
@@ -1,10 +1,19 @@
 import { redirect } from "next/navigation";
+import { PageProps } from "@/lib/@types/pageProps";
+import { getAllPlugins } from "@/lib/getPluginsData";
 
 type Props = {
-  params: { path: string };
+  params: PageProps["params"] & { path: string };
 };
 
-export default function PluginPageRedirect({ params: { path } }: Props) {
-  redirect(`/en/plugin/${path}`);
-  return null;
+export async function generateStaticParams() {
+  const plugins = getAllPlugins("en");
+  return plugins.map((plugin) => ({
+    locale: "en",
+    path: plugin.path,
+  }));
+}
+
+export default async function PluginPageRedirect({ params }: Props) {
+  redirect(`/en/plugin/${params.path}`);
 }
